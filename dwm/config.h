d429dabaa7f0d7faa28c4bdc508e8428511c2b89
AMF064 <114866865+AMF064@user.noreply.github.com>
2022-10-30 09:07:52 +0100

    Newest tweaks

--- dwm/config.h
+++ dwm/config.h
@@ -92,9 +92,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_plus,   setgaps,         {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	{ MODKEY|ShiftMask,             XK_l,  setgaps,        {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_p,   setgaps,        {.i = +1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
