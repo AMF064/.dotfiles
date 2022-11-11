d1baf4adf8d42fac629482fbd731a1026c479783
AMF064 <114866865+AMF064@user.noreply.github.com>
2022-11-11 22:43:38 +0100

    Qutebrowser tweaks

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -10,12 +10,6 @@
 #   qute://help/configuring.html
 #   qute://help/settings.html
 
-#Libraries
-import subprocess
-import os
-from qutebrowser.api import interceptor
-
-
 # Change the argument to True to still load settings configured via autoconfig.yml
 config.load_autoconfig(False)
 
@@ -143,18 +137,45 @@ c.colors.downloads.bar.bg = 'black'
 c.colors.webpage.darkmode.algorithm = 'lightness-cielab'
 
 #Dark mode if possible
-c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.darkmode.policy.images = "never"
+c.colors.webpage.preferred_color_scheme = "dark"
 
-##Youtube ad blocking
-def filter_yt(info: interceptor.Request):
-    #Block the given request if necessary
-    url = info.request_url
-    if(
-        url.host() == "www.youtube.com"
-        and url.path() == "/get_video_info"
-        and "&adformat=" in url.query()
-    ):
-        info.block()
+#Ad blocking
+c.content.blocking.method='both'
+c.content.blocking.adblock.lists = [
+        "https://easylist.to/easylist/easylist.txt",
+        "https://easylist.to/easylist/easyprivacy.txt",
+        "https://secure.fanboy.co.nz/fanboy-annoyance.txt",
+        "https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt",
+        "https://pgl.yoyo.org/adservers/serverlist.php?showintro=0;hostformat=hosts",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2021.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
+        "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt",
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt"
+        ]
 
+##Keybindings
+config.bind("<p><t>", "tab-pin")
+config.bind(";w", "hint links spawn --detach mpv --force-window yes {hint-url}")
 
-interceptor.register(filter_yt)
+#No content autoplay and show PDFs in the browser
+c.content.autoplay = False
+c.content.pdfjs = True
+
+#Downloads directory
+c.downloads.location.directory = "/home/amf/Downloads/browser/"
+
+#Fonts work fine, so no need to set them up yet!
+
+#Remove finished downloads
+c.downloads.remove_finished = 5
+
+#No cookies, please
+c.content.cookies.store = False
