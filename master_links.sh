86b42bd31049911c21493243224307fc4b0b84cc
AMF064 <114866865+AMF064@user.noreply.github.com>
2022-12-04 21:21:13 +0100

    Install scripts

--- master_links.sh
+++ master_links.sh
@@ -1,3 +1,5 @@
+#Remove old files
+rm /home/amf/.bashrc
 
 #Make directories
 mkdir /home/amf/.config /home/amf/.config/qutebrowser /home/amf/.config/sxhkd /home/amf/.config/vifm
@@ -7,9 +9,9 @@ mkdir -p /home/amf/Pictures/wallpapers
 ln -s /home/amf/.dotfiles/.xinitrc /home/amf/.xinitrc
 ln -s /home/amf/.dotfiles/.bashrc /home/amf/.bashrc
 
-ln -s /home/amf/.dotfiles/Programs/manq /usr/local/bin/manq
-ln -s /home/amf/.dotfiles/Programs/fileman /usr/local/bin/fileman
-ln -s /home/amf/.dotfiles/Programs/wallpaper /usr/local/bin/wallpaper
+sudo ln -s /home/amf/.dotfiles/Programs/manq /usr/local/bin/manq
+sudo ln -s /home/amf/.dotfiles/Programs/fileman /usr/local/bin/fileman
+sudo ln -s /home/amf/.dotfiles/Programs/wallpaper /usr/local/bin/wallpaper
 
 ln -s /home/amf/.dotfiles/qutebrowser/config.py /home/amf/.config/qutebrowser/config.py
 ln -s /home/amf/.dotfiles/sxhkd/sxhkdrc /home/amf/.config/sxhkd/sxhkdrc
