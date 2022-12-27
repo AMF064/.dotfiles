f265b5e13606fbc9b09e821f2a781e318b7d9ebe
AMF064 <114866865+AMF064@user.noreply.github.com>
2022-12-27 14:31:36 +0100

    Neovim config in Lua

---
+++ nvim/lua/amf/remap.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)	--Open netrw
+vim.keymap.set("n", "<C-l>", vim.cmd.buffers)	--List buffers
+vim.keymap.set("n", "<leader>ba", vim.cmd('badd .'))		--Add buffer
+vim.keymap.set("n", "<leader>bn", vim.cmd.bnext)	--Next buffer
+vim.keymap.set("n", "<leader>bp", vim.cmd.bprevious)	--Previous buffer
+vim.keymap.set("n", "<C-w>", vim.cmd.bdelete)		--Delete buffer
