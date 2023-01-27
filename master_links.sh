25878dc4166c9435a028cd80acb0bda79aa831d1
AMF064 <114866865+AMF064@users.noreply.github.com>
2023-01-28 00:18:52 +0100

    Keymaps

--- master_links.sh
+++ master_links.sh
@@ -18,6 +18,7 @@ sudo ln -s /home/amf/.dotfiles/Programs/wallpaper /usr/local/bin/wallpaper
 ln -s /home/amf/.dotfiles/qutebrowser/config.py /home/amf/.config/qutebrowser/config.py
 ln -s /home/amf/.dotfiles/sxhkd/sxhkdrc /home/amf/.config/sxhkd/sxhkdrc
 ln -s /home/amf/.dotfiles/vifm/vifmrc /home/amf/.config/vifm/vifmrc
+ln -s /home/amf/.dotfiles/vifm/colors /home/amf/.config/vifm/colors/
 ln -s /home/amf/.dotfiles/nvim/* /home/amf/.config/nvim/
 ln -s /home/amf/.dotfiles/mpd/mpd.conf /home/amf/.config/mpd/mpd.conf
 ln -s /home/amf/.dotfiles/ncmpcpp/* /home/amf/.config/ncmpcpp/
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -99,6 +99,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/amf/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  ["vim-fugitive"] = {
+    loaded = true,
+    path = "/home/amf/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    url = "https://github.com/tpope/vim-fugitive"
   }
 }
 
