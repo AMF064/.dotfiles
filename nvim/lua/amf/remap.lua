9125c957e0894d27325a056f6151c5963720452a
AMF064 <114866865+AMF064@users.noreply.github.com>
2023-01-29 14:32:10 +0100

    New remap

--- nvim/lua/amf/remap.lua
+++ nvim/lua/amf/remap.lua
@@ -1,10 +1,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)	--Open netrw
 vim.keymap.set("n", "<M-l>", vim.cmd.buffers)	--List buffers
-vim.keymap.set("n", "<C-b>", ":buffer ")	--Go to buffer
+vim.keymap.set("n", "<C-b>", ":buffer <C-d>")	--Go to buffer
 vim.keymap.set("n", "<leader>ba", vim.cmd('badd .'))		--Add buffer
 vim.keymap.set("n", "<leader>bn", vim.cmd.bnext)	--Next buffer
 vim.keymap.set("n", "<leader>bp", vim.cmd.bprevious)	--Previous buffer
+vim.keymap.set("n", "<leader>q", vim.cmd.bunload)	     --Previous buffer you visited
 vim.keymap.set("n", "<M-d>", vim.cmd.bdelete)		--Delete buffer
 vim.keymap.set("n", "<M-t>", vim.cmd.terminal)		--Open terminal
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")         --Exit terminal mode
