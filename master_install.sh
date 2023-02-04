cde1d9ab0f3e93d79cf1eea2378e4a6c0059d947
AMF064 <114866865+AMF064@users.noreply.github.com>
2023-02-05 00:19:00 +0100

    Added the Brave adblocker for qutebrowser

--- master_install.sh
+++ master_install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #Install necessary packages
-pacman -S xorg-server xorg-xinit xorg-xsetroot xorg-xrandr libxft ttf-liberation ttf-dejavu qutebrowser youtube-dl vifm dmenu zathura zathura-pdf-mupdf inetutils neovim mpv alsa-utils sxhkd feh sxiv mpd ncmpcpp xclip
+pacman -S xorg-server xorg-xinit xorg-xsetroot xorg-xrandr libxft ttf-liberation ttf-dejavu qutebrowser youtube-dl vifm dmenu zathura zathura-pdf-mupdf inetutils neovim mpv alsa-utils sxhkd feh sxiv mpd ncmpcpp xclip python-adblock
