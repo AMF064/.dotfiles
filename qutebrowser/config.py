18aecbd1cdfadf8c1829dd61dc039a529c8206e6
AMF064 <114866865+AMF064@users.noreply.github.com>
2023-02-04 23:48:04 +0100

    Nothing changed

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -145,9 +145,9 @@ c.editor.command = ["nvim" , "+{line}", "{file}"]
 
 #Fileselect commands
 c.fileselect.handler = "external"
-c.fileselect.folder.command = ["st", "-e", "vifm", "--choose-dir {}", "~", "~"]
-c.fileselect.multiple_files.command = ["st", "-e", "vifm", "--choose-files {}", "~", "~"]
-c.fileselect.single_file.command = ["st", "-e", "vifm", "--choose-files {}", "~", "~"]
+c.fileselect.folder.command = ["st", "-e", "vifm", "--choose-dir={}", "~", "~"]
+c.fileselect.multiple_files.command = ["st", "-e", "vifm", "--choose-files={}", "~", "~"]
+c.fileselect.single_file.command = ["st", "-e", "vifm", "--choose-files={}", "~", "~"]
 
 #Ad blocking
 c.content.blocking.method='both'
