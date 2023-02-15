e4e4e0dceedd65689b9704c9b357edaca7985d4e
AMF064 <114866865+AMF064@users.noreply.github.com>
2023-02-15 12:14:02 +0100

    Always spawn qutebrowser in tag 4

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -32,6 +32,8 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+    { "qutebrowser",NULL,     NULL,       1 << 3,       0,           -1 },
+    { "Floating", "st-256color",NULL,     0,            1,           -1 },
 };
 
 /* layout(s) */
