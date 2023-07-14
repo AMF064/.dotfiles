001d62706af6971ce133d29bf20a7d05e9e2e663
AMF064 <114866865+AMF064@users.noreply.github.com>
2023-07-14 12:22:12 +0200

    Telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,20 +1,24 @@
---require('telescope').setup{
-    --defaults = {
-        --vimgrep_arguments = {
-            --"grep",
-            --"--color=never",
-            --"-l"
-        --}
-    --}
---}
-
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>cf', function()
     builtin.find_files({ cwd = "/home/amf/.config/", prompt = "##DOTFILES##",  shorten_path = true,  hidden = true, follow = true })
 end)
+vim.keymap.set('n', '<leader>ev', function()
+    builtin.find_files({ cwd = "/home/amf/.config/nvim/lua/amf/", prompt = "##CONFIG##",  shorten_path = true,  hidden = true, follow = true })
+end)
 vim.keymap.set('n', '<C-b>', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+--require('telescope').setup{
+    --    defaults = {
+        --        vimgrep_arguments = {
+            --            "grep",
+            --            "-n",
+            --            "-l",
+            --            "--color=never",
+            --        }
+            --    }
+            --}
