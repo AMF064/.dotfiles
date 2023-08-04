84cbcc2cf26cb5d28884646c347ddc3939c1c384
AMF064 <114866865+AMF064@users.noreply.github.com>
2023-08-04 16:05:15 +0200

    Zepl installed

--- nvim/lua/amf/packer.lua
+++ nvim/lua/amf/packer.lua
@@ -23,5 +23,6 @@ return require('packer').startup(function(use)
 
     use({'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}})
     use({'tpope/vim-fugitive'})
+    use({'axvr/zepl.vim'})
 
 end)
