de378f0ec5aa1ff9c0cdd220b57fdcb6b50fbed8
AMF064 <114866865+AMF064@users.noreply.github.com>
2023-09-16 13:31:15 +0200

    Make master_install more generic Change package manager depending on the distro

--- master_install.sh
+++ master_install.sh
@@ -2,5 +2,16 @@
 
 PROGRAM_LIST=$(cat PROGRAMS)
 
+INSTALL=""
+case "$(hostnamectl | awk '/Operating System/{print $3}')" in
+    Arch)
+        INSTALL="pacman -S"
+        ;;
+    Debian)
+        INSTALL="apt-get install"
+        ;;
+
+    esac
+
 #Install packages in MANIFEST
-pacman -S $PROGRAM_LIST
+$INSTALL $PROGRAM_LIST
