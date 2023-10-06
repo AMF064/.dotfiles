87637ec493f4693f2d08be6b1393afb8b446cd2f
AMF064 <114866865+AMF064@users.noreply.github.com>
2023-10-06 22:46:56 +0200

    Add OpenBSD support

--- master_install.sh
+++ master_install.sh
@@ -3,27 +3,33 @@
 PROGRAM_LIST=$(cat PROGRAMS)
 
 INSTALL=""
-case "$(hostnamectl | awk '/Operating System/{print $3}')" in
-    Arch)
-        INSTALL="pacman -S"
+case "$(uname)" in
+    OpenBSD)
+        INSTALL="pkg_add"
         ;;
-    Parabola)
-        INSTALL="pacman -S"
+    Linux)
+        case "$(hostnamectl | awk '/Operating System/{print $3}')" in
+            Arch)
+                INSTALL="pacman -S"
+                ;;
+            Parabola)
+                INSTALL="pacman -S"
+                ;;
+            Alpine)
+                INSTALL="apk add"
+                ;;
+            Void)
+                INSTALL="xbps-install"
+                ;;
+            Gentoo)
+                INSTALL="emerge -atv"
+                ;;
+            *)
+                INSTALL="apt-get install"
+                ;;
+        esac
         ;;
-    Alpine)
-        INSTALL="apk add"
-        ;;
-    Void)
-        INSTALL="xbps-install"
-        ;;
-    Gentoo)
-        INSTALL="emerge -atv"
-        ;;
-    *)
-        INSTALL="apt-get install"
-        ;;
-
-    esac
+esac
 
 #Install packages in PROGRAMS
 $INSTALL $PROGRAM_LIST
