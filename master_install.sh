a784a81e3cd152b99286982d0c05c4893dbffdb2
AMF064 <114866865+AMF064@users.noreply.github.com>
2024-03-27 22:23:33 +0100

    Added text editor script

--- master_install.sh
+++ master_install.sh
@@ -34,3 +34,29 @@ esac
 
 #Install packages in PROGRAMS
 $INSTALL $PROGRAM_LIST
+
+VIM_REPO="git@github.com:AMF064/dotvim.git"
+NVIM_REPO="git@notabug.org:AMF064/dotnvim.git"
+EMACS_REPO="git@notabug.org:AMF064/.emacs.d.git"
+
+VIM_PATH="$HOME/.vim"
+NVIM_PATH="$HOME/.config/nvim"
+EMACS_PATH="$HOME/.emacs.d"
+
+prompt="Choose an editor:\n1) vim\n2) neovim\n3) emacs\nOther characters will cancel this operation.\nYour choice (default = 1): ";
+
+printf "$prompt"
+read editor
+    case $editor in
+        1|"")
+            $INSTALL vim
+            git clone $VIM_REPO $VIM_PATH
+            ;;
+        2)
+            $INSTALL neovim
+            git clone $NVIM_REPO $NVIM_PATH
+            ;;
+        3)
+            $INSTALL emacs
+            git clone $EMACS_REPO $EMACS_PATH
+            ;;
